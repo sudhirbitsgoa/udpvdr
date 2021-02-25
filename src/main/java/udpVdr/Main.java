ca0736d6b1fec0bb26da75886de970a74add219f
sudhir <sudhirbitsgoa@gmail.com>
2021-02-25 09:05:35 +0530

    working

--- src/main/java/udpVdr/Main.java
+++ src/main/java/udpVdr/Main.java
@@ -31,6 +31,9 @@ public class Main {
    private static String MULTICAST_INTERFACE;
    private static int MULTICAST_PORT;
    private static String MULTICAST_IP;
+   public final static char CR  = (char) 0x0D;
+   public final static char LF  = (char) 0x0A; 
+   public final static char NULL = (char) 0X00;
    
    public void sendMessage(String ip, String iface, int port,
          String message) throws IOException {
@@ -40,6 +43,9 @@ public class Main {
          .getByName(iface);
       datagramChannel.setOption(StandardSocketOptions
          .IP_MULTICAST_IF,networkInterface);
+      // System.out.println(message.getBytes());
+      message = message + CR + LF;
+      // System.out.println(message.getBytes());
       ByteBuffer byteBuffer=ByteBuffer.wrap
          (message.getBytes());
       InetSocketAddress inetSocketAddress=new
@@ -75,9 +81,13 @@ public class Main {
         Main mp = new Main();
         // String msg1 = "$GPRMC,132537,A,3753.2515,N,02429.2311,E,19956.8,19.4,130121,5,E,A*3A";
         String msg1 = args[3];
+        if (args.length == 5 && "nullChar".equals(args[4])) {
+            String[] arr = msg1.split("UdPbC");
+            msg1 = "UdPbC" + NULL + arr[1];
+        }
         // mp.multicast("$GPRMC,132537,A,3753.2515,N,02429.2311,E,19956.8,19.4,130121,5,E,A*3A");
         mp.sendMessage(MULTICAST_IP,MULTICAST_INTERFACE,
          MULTICAST_PORT, msg1);
-        System.out.println(args[3] + " multicast to "+ args[2] +" on port " + args[1]);
+        System.out.println(msg1 + " multicast to "+ args[2] +" on port " + args[1]);
    }
 }
